1f1caf44a39249aa9496bc0fa2bb378345a3f43f
lostgirrlll <13521030@std.stei.itb.ac.id>
2023-11-16 17:22:35 +0700

    feat: edit main

--- soap-service/src/main/java/com/jahitin/Main.java
+++ soap-service/src/main/java/com/jahitin/Main.java
@@ -1,13 +1,15 @@
 package com.jahitin;
 
+import com.jahitin.service.TestingService;
+
+import java.util.concurrent.Flow.Subscription;
+
 import javax.xml.ws.Endpoint;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://0.0.0.0:8003/ws/testing", new TestingService());
+            Endpoint.publish("http://0.0.0.0:8003/ws/testing", new Subscription());
             System.out.println("Service is started");
 
         }
