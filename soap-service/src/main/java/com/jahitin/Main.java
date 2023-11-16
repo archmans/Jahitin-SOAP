8c0324088d5ad9a5c2d30b32216df5da19fb3556
lostgirrlll <13521030@std.stei.itb.ac.id>
2023-11-16 18:12:52 +0700

    feat: edit import in main

--- soap-service/src/main/java/com/jahitin/Main.java
+++ soap-service/src/main/java/com/jahitin/Main.java
@@ -1,15 +1,13 @@
 package com.jahitin;
 
-import com.jahitin.service.TestingService;
-
-import java.util.concurrent.Flow.Subscription;
+import com.jahitin.service.Subscription;
 
 import javax.xml.ws.Endpoint;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://0.0.0.0:8003/ws/testing", new Subscription());
+            Endpoint.publish("http://0.0.0.0:8003/ws/subscription", new Subscription());
             System.out.println("Service is started");
 
         }
